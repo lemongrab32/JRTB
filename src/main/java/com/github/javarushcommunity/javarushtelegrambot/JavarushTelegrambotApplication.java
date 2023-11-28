b703e7798cdffa16ddd4ac37fa0e5d74681d78e5
IO3683 <nu-dopustim@mail.ru>
2023-11-28 15:13:01 +0600

    JRTB-2: added telegram bot to the project

--- src/main/java/com/github/javarushcommunity/javarushtelegrambot/JavarushTelegrambotApplication.java
+++ src/main/java/com/github/javarushcommunity/javarushtelegrambot/JavarushTelegrambotApplication.java
@@ -2,9 +2,11 @@ package com.github.javarushcommunity.javarushtelegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class JavarushTelegrambotApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavarushTelegrambotApplication.class, args);
